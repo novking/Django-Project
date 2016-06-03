a5a861aaba2cb0b20c284bc4366abd6c555f877e
Yiming Zhang <awesome1aaron@gmail.com>
2016-06-02 22:23:53 -0400

    tried my own
    not bad. remember extends. need "s"
    {% block content%}
    {%endblock%}

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'personal',
+    'main_street_gym',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', include ('personal.urls')),
+    url(r'^msg', include ('main_street_gym.urls')),
 ]
