1a64ce171b24f4f5d3d2e0acb54a4c4e43d82a0a
Yiming Zhang <awesome1aaron@gmail.com>
2016-06-03 21:29:21 -0400

    Interesting
    basically. for urls. href is different. if you want to have an header,
    which have a navbar in it. then you should make urls parallel instead of
    nest.  if home = a/. blog = a/b. . then when it jump to blog. it will
    never jump back up

--- mysite/personal/urls.py
+++ mysite/personal/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^contact/', views.contact, name='contact')
+    url(r'^contact/$', views.contact, name='contact')
 ]
